cca7142efa817f53f81a732cf7605b80dece78bb
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-19 00:21:50 -0500

    Added script to start server.js
    Added env variable for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 let app = express();
 
 // Add support for partials.
@@ -50,6 +51,6 @@ app.get('/about', (req, res) => {
         pageTitle: 'About Page'
     });
 });
-app.listen(3000, () => {
-    console.log('Server running on port 3000.');
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}.`);
 });

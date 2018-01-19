f1b722e4f54f176ade03e666980a8e4af1e1235e
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-18 19:52:32 -0500

    More Express
    Added hbs view templating for Express.JS.
    
    Added more express examples to server.js.
    
    Created partials and other templates with hbs.

--- server.js
+++ server.js
@@ -1,24 +1,53 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
+// Add support for partials.
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let currentTime = new Date().toString();
+    let log = `${currentTime}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    // fs.appendFile('serverLog', log + '\n', (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    // });
+    next();
+});
+
+// No next call blocks running of other middleware.
+// app.use((req, res, next) => {
+//     res.render('maintanence.hbs');
+// });
+
 app.use(express.static(__dirname + '/public-src'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('loudFunc', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         title: 'Home Page',
         pageTitle: 'Home',
-        welcomeMessage: 'Lorem Ipsum Dipsum',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Lorem Ipsum Dipsum'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 app.listen(3000, () => {
